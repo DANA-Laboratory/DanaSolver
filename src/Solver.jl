37681073c117036ccae02041fb5702b0ef92ed20
dana-laboratory <rafzalan@gmail.com>
2015-01-27 20:07:46 +0330

    someone wants to delete rref

--- src/Solver.jl
+++ src/Solver.jl
@@ -113,4 +113,37 @@ module Solver
     rreModel=rref(vals)
     return rreModel,vars,nolinearFunctions,nolinearArgs
   end
+  function rref(U::Array{Float64,2})		
+    nr, nc = size(U)		
+    e = eps(norm(U,Inf))		
+    i = j = 1		
+    while i <= nr && j <= nc		
+      (m, mi) = findmax(abs(U[i:nr,j]))		
+      mi = mi+i - 1		
+      if m <= e		
+        U[i:nr,j] = 0		
+        j += 1		
+      else		
+        for k=j:nc		
+          U[i, k], U[mi, k] = U[mi, k], U[i, k]		
+        end		
+        d = U[i,j]		
+        for k = j:nc		
+          U[i,k] /= d		
+        end		
+        for k = 1:nr		
+          if k != i		
+            d = U[k,j]		
+            for l = j:nc		
+              U[k,l] -= d*U[i,l]		
+            end		
+          end		
+        end		
+        i += 1		
+        j += 1		
+      end		
+    end		
+    U		
+  end		
+  rref(x::Number) = one(x)
 end

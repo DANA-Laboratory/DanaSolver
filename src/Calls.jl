191b8eefe5c7997b7dd86ba017b095b096962eb4
Reza Afzalan <RAfzalan@gmail.com>
2015-02-04 14:50:31 +0330

    seperate call to Optomiae

--- src/Calls.jl
+++ src/Calls.jl
@@ -27,3 +27,30 @@ function callfzero(fun::Function,gus::Float64,br::Vector{Float64},maxattempts=30
     rethrow(er)
   end
 end
+function callmin(eqGroup::Vector{Function},indexGroup::Vector{Tuple},lo::Vector{Float64},up::Vector{Float64},de::Vector{Float64})
+  numberOfEquations=length(nonliFuns)
+  somethingUpdated=false
+  opt = Opt(:GN_DIRECT_L, length(eqGroup))
+  lower_bounds!(opt, lo)
+  upper_bounds!(opt, up)
+  stopval!(opt, 1.0e-12)
+  maxtime!(opt, 1.0*length(eqGroup))
+  ftol_abs!(opt, 1.0e-19)
+  ftol_rel!(opt, 1.0e-18)
+  optfun=(y,gradient)->begin
+    try
+      mapreduce(x->(eqGroup[x](getindex(y,indxGroup[x])...))^2,+,[1:i])
+    catch er
+      println("in nonlinear optimize , fail with following vals: ",y)
+      rethrow(er)
+    end
+  end
+  min_objective!(opt,optfun)
+  (minf,minx,ret)=optimize(opt,de)
+  if "$ret"=="STOPVAL_REACHED"
+    return minx
+  elseif "$ret"=="MAXTIME_REACHED"
+    println("NLopt fail to MAXTIME_REACHED")
+    return nothing
+  end
+end
